de9a137b6f1bbc0367f3fd7e0e49fe80c6fef8a8
Laura-Moro <Laura.moro96@gmail.com>
2022-03-23 16:56:46 +0100

    Update life zone forest cover.R

--- life zone forest cover.R
+++ life zone forest cover.R
@@ -2,16 +2,16 @@ library(raster)
 library(rgdal)
 
 # Load Helmer age map
-age <- raster("/Users/au529793/Projects/Puerto Rico/GIS Data/PR_Forest_Age/pr_forestage_zone_v2/iitf_jgr113_puertorico_forestage_zone_reprojectedWGS84.tif")
+age <- raster("/Users/lauramoro/Documents/PUERTO_RICO/Forest_Age_Helmer/Maps_1951-2000/iitf_jgr113_puertorico_forestage_zone_reprojectedWGS84.tif")
 
 ### READ IN DATA
-r51 <- raster("/Users/au529793/Downloads/prchange_landcov_51to2000/puerto51_sub1_100905_landcov_final.img")
+r51 <- raster("/Users/lauramoro/Documents/PUERTO_RICO/Forest_Age_Helmer/Maps_1951-2000/puerto51_sub1_100905_landcov_final.img")
 
-r77 <- raster("/Users/au529793/Downloads/prchange_landcov_51to2000/puerto77_sub1_100905_landcov_urbveg_final.img")
+r77 <- raster("/Users/lauramoro/Documents/PUERTO_RICO/Forest_Age_Helmer/Maps_1951-2000/puerto77_sub1_100905_landcov_urbveg_final.img")
 
-r91 <- raster("/Users/au529793/Downloads/prchange_landcov_51to2000/pr91_100805_final_quarry_recode2landcov_subset.img")
+r91 <- raster("/Users/lauramoro/Documents/PUERTO_RICO/Forest_Age_Helmer/Maps_1951-2000/pr91_100805_final_quarry_recode2landcov_subset.img")
 
-r00 <- raster("/Users/au529793/Downloads/prchange_landcov_51to2000/pr2000_100805_final_quarry_recode2landcov_subset.img")
+r00 <- raster("/Users/lauramoro/Documents/PUERTO_RICO/Forest_Age_Helfmer/Maps_1951-2000/pr2000_100805_final_quarry_recode2landcov_subset.img")
 
 ### CROP AND REPROJECT
 rp51 <- projectRaster(r51, age, method='ngb')
@@ -25,12 +25,15 @@ f77 <- rp77 %in% c(5,7)
 f91 <- rp91 %in% c(5,7)
 f00 <- rp00 %in% c(5,7)
 
-f <- stack(f51, f77, f91, f00)
+plot <- stack(f51, f77, f91, f00)p
+
+
+
 
 
 ## USING RASTER
 ### READ LIFE ZONE AND RASTERIZE
-lz <- readOGR("/Users/au529793/Projects/Puerto Rico/GIS Data/PR_Lifezone/Lifezones/lifezones_Project.shp")
+lz <- readOGR("/Users/lauramoro/Documents/PUERTO_RICO/Land_Cover_GAP/Data/Lifezones/lifezones_Project.shp")
 
 lzr <- rasterize(lz['ECOZONE'], age)
 
@@ -45,7 +48,7 @@ colnames(result_raster) <- c(1951, 1977, 1991, 2000)
 
 for(i in 1:length(unique(lzr@data@attributes[[1]]$lz))){
   print(i)
-  tmp <- f * (lzr2 == i)
+  tmp <- f *(lzr2 == i)
   
   # TOTAL NUMBER OF PIXELS
   result_raster[i,] <- cellStats(tmp, sum)
@@ -57,26 +60,30 @@ for(i in 1:length(unique(lzr@data@attributes[[1]]$lz))){
 plot(c(1951, 1977, 1991, 2000), result_raster[1,], 
      type='l', ylim=c(0, max(result_raster)), lwd=3)
 
+
 for(i in 2:6){
   lines(c(1951, 1977, 1991, 2000), result_raster[i,], col=i, lwd=3)
 }
 
-legend("left", legend=rownames(result_raster), col=1:6, lty=1, bty='n', cex=.75, lwd=3)
+
+legend("topleft", legend=rownames(result_raster), col=1:6, lty=1, bty='n', cex=.75, lwd=3)
 
 
 
 ### USING SHAPEFILES
 # Get results using the polygons to mask different areas
-result <- matrix(nrow=length(unique(lz2$ECOZONE)), ncol=4)
-rownames(result) <- unique(lz2$ECOZONE)
+
+lz <- readOGR("/Users/lauramoro/Documents/PUERTO_RICO/Land_Cover_GAP/Data/Lifezones/lifezones_Project.shp")
+
+result <- matrix(nrow=length(unique(lz$ECOZONE)), ncol=4)
+rownames(result) <- unique(lz$ECOZONE)
 colnames(result) <- c(1951, 1977, 1991, 2000)
 
-for(i in 1:length(unique(lz2$ECOZONE))){
-  focal_lz <- unique(lz2$ECOZONE)[i]
-  tmp <- mask(f, lz2[lz2$ECOZONE == focal_lz,])
+for(i in 1:length(unique(lz$ECOZONE))){
+  focal_lz <- unique(lz$ECOZONE)[i]
+  tmp <- mask(f, lz[lz$ECOZONE == focal_lz,])
   result[i,] <- cellStats(tmp, sum)
 }
-
 
 plot(c(1951, 1977, 1991, 2000), result[1,], 
      type='l', ylim=c(0,max(result)), lwd=3)
